fbdb3a4c8273b52625772a1702a3a2d53853c039
CalendaLucia <calendalucia4@gmail.com>
2022-12-19 19:37:09 +0100

    fine style

--- js/scripts.js
+++ js/scripts.js
@@ -9,27 +9,27 @@ document.getElementById("età").innerHTML = età;
 
 const prezzoAlChilomentro = 0.21;
 
-const prezzoFinale = chilometri * prezzoAlChilomentro;
+const prezzoFinale = parseInt(chilometri) * prezzoAlChilomentro;
 document.getElementById("result").innerHTML = prezzoFinale + "€"; 
 
 const scontoUnder_18 = prezzoFinale  / 100 * 20 ;
 
 const scontoOver_65 = prezzoFinale / 100 * 40 ;
 
-const prezzoUnder_18 = scontoUnder_18 - prezzoFinale ;
+const prezzoUnder_18 = prezzoFinale - scontoUnder_18 ;
 
-const prezzoOver_65 =  scontoOver_65 - prezzoFinale;
+const prezzoOver_65 = prezzoFinale - scontoOver_65 ;
 
 
 
 
 if (età < 18) {
     document.getElementById("sconto").innerHTML = "20%";
-    document.getElementById("result").innerHTML = prezzoUnder_18 + "€"; 
+    document.getElementById("result").innerHTML = prezzoUnder_18.toFixed(2) + "€"; 
     
 } else if (età >= 65) {
    document.getElementById("sconto").innerHTML = "40%";
-   document.getElementById("result").innerHTML = prezzoOver_65 + "€";
+   document.getElementById("result").innerHTML = prezzoOver_65.toFixed(2) + "€";
 
 }  else {
     document.getElementById("sconto").innerHTML = "No";
